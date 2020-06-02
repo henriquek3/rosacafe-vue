62c8dc5e940bf701e718b1f4d8ed41803d319213
Jean Freitas <henriquek3@live.com>
2020-06-02 16:41:01 -0400

    login ok

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
+import {passportConfig} from '../../.env'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {},
+    mutations: {},
+    actions: {
+        login(context, {email, password}) {
+            const data = {
+                password,
+                username: email,
+                grant_type: passportConfig.grantType,
+                client_id: passportConfig.clientId,
+                client_secret: passportConfig.clientSecret,
+                scope: passportConfig.scope,
+            };
+            axios.defaults.withCredentials = true;
+            axios.post('http://localhost:8000/oauth/token', data)
+                .then(response => {
+                    sessionStorage.setItem('token', JSON.stringify(response.data));
+                })
+                .catch(error => console.log(error))
+            ;
+        },
+    },
+    modules: {}
 })
